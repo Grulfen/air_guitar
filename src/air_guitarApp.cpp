ba1c08a82fb57bb8653aa60e00d39d482a7de9f2
Carlos Bergen <hola@carlosbergen.com>
2013-12-26 17:08:30 -0300

    Added UI

--- src/air_guitarApp.cpp
+++ src/air_guitarApp.cpp
@@ -1,6 +1,5 @@
 // Includes
 #include <Windows.h>
-#include "NuiApi.h"
 #include "cinder/app/AppBasic.h"
 #include "cinder/Camera.h"
 #include "cinder/gl/Texture.h"
@@ -10,11 +9,15 @@
 #include "cinder/Utilities.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Text.h"
+#include "NuiApi.h"
 #include "moduleCapture.h"
 #include "moduleProcessing.h"
 #include "modulePresentation.h"
-
-
+#include "ciUI.h" 
+/* 
+* This application demonstrates how to acquire and display 
+* skeleton data.
+*/ 
 class AirGuitarApp : public ci::app::AppBasic 
 {
 
@@ -23,10 +26,11 @@ public:
 	// Cinder callbacks
 	void	draw();
 	void	keyDown( ci::app::KeyEvent event );
-	void	prepareSettings( ci::app::AppBasic::Settings *settings );
+//	void	prepareSettings( ci::app::AppBasic::Settings *settings );
 	void	setup();
 	void	shutdown();
 	void	update();
+	void	guiEvent(ciUIEvent *event);
 
 	moduleCapture capture;
 
@@ -34,13 +38,17 @@ private:
 	modulePresentation mPresentation;
 	moduleProcessing mProcessing;
 	HandsHip *handsHip;
-
-	ci::gl::TextureFontRef mFont;
+	float guiWidth; 
+    float guiHeight; 
+    float dim; 
+    float length; 
 	ci::gl::Texture *backgroundTex;
-	std::string mText;
-	point chordHandPos;
-	point playHandPos;
-	point HipPos;
+    
+    ColorA backgroundColor; 
+    ciUICanvas *gui; 
+	int instrument;
+	bool rightHanded;
+
 };
 
 // Imports
@@ -51,16 +59,16 @@ using namespace std;
 // Render
 void AirGuitarApp::draw()
 {
-	ci::gl::clear();
+	ci::gl::clear(backgroundColor);
+	
 	if(backgroundTex != NULL){
 		gl::draw(*backgroundTex, getWindowBounds());
 	}
-	mFont->drawString(mText, cinder::Rectf(0,0,800,200));
-	if(handsHip != NULL){
-		ci::gl::drawSolidCircle( Vec2f( chordHandPos.x, chordHandPos.y), 10);
-		ci::gl::drawSolidCircle( Vec2f( playHandPos.x, playHandPos.y), 10);
-		ci::gl::drawSolidCircle( Vec2f( HipPos.x, HipPos.y), 10);
-	}
+	 gl::enableAlphaBlending();
+	gui->draw();
+	 gl::disableAlphaBlending();
+	
+//	mFont->drawString(mText, cinder::Rectf(0,0,800,200));
 }
 
 // Handles key press
@@ -75,24 +83,83 @@ void AirGuitarApp::keyDown( KeyEvent event )
 	case KeyEvent::KEY_f:
 		setFullScreen( !isFullScreen() );
 		break;
+	case KeyEvent::KEY_SPACE:
+		/*if(mText == "hejsan"){
+			mText = "apfan";
+		} else {
+			mText = "hejsan";
+		}*/
+		break;
 	}
 }
 
 // Prepare window
-void AirGuitarApp::prepareSettings( Settings *settings )
+/*void AirGuitarApp::prepareSettings( Settings *settings )
 {
         settings->setWindowSize( 800, 600 );
         settings->setFrameRate( 60.0f );
-}
+}*/
 
 // Set up
 void AirGuitarApp::setup(){
-	mFont = ci::gl::TextureFont::create(Font("Arial Black", 40));
-	mText = "durr";
-	capture = moduleCapture();
 	backgroundTex = NULL;
+     
+    backgroundColor = ColorA(233.0/255.0, 52.0/255.0, 27.0/255.0);     
+    guiWidth = getWindowWidth()*.4; 
+    guiHeight= getWindowHeight()*.75; 
+    dim = guiWidth*.0625; 
+    length = guiWidth-CI_UI_GLOBAL_WIDGET_SPACING*2;
+
+	rightHanded = true;
+
+	vector<string> instruments;
+	instruments.push_back("Electric Guitar");
+	instruments.push_back("Bass");
+
+
+	vector<string> handedness;
+	handedness.push_back("Right");
+	handedness.push_back("Left");
+	
+    
+    gui = new ciUICanvas(10,10,guiWidth, guiHeight);
+	gui->addWidgetDown(new ciUILabel(0,25, "AIRGUITAR", CI_UI_FONT_MEDIUM)); 
+	gui->addWidgetDown(new ciUISlider(length - CI_UI_GLOBAL_WIDGET_SPACING, dim, 0.0, 1.0, 1.0, "Volume")); 
+	gui->addWidgetDown(new ciUIRadio(20.0,20.0,"Instrument",instruments,0));
+	((ciUIRadio *) gui->getWidget("Instrument"))->activateToggle("Electric Guitar");
+	gui->addWidgetDown(new ciUIRadio(20.0,20.0,"Handedness",handedness,0));
+	((ciUIRadio *) gui->getWidget("Handedness"))->activateToggle("Right");
+   
+    gui->registerUIEvents(this, &AirGuitarApp::guiEvent);
+
+	capture = moduleCapture();
 	mPresentation.setSounds(0);
-	OutputDebugStringW(L"In setup end\n");
+}
+
+void AirGuitarApp::guiEvent(ciUIEvent *event){
+	string name = event->widget->getName();
+
+	if(name == "Volume")
+	{
+		ciUISlider *slider = (ciUISlider *) event->widget; 
+		mPresentation.globalVolume = slider->getScaledValue(); 
+	}
+	if(name == "Electric Guitar")
+	{	
+		mPresentation.setSounds(0);
+	}
+	if(name == "Bass")
+	{	
+		mPresentation.setSounds(1);
+	}
+	if(name == "Right")
+	{
+		rightHanded = true;
+	}
+	if(name == "Left")
+	{
+		rightHanded = false;
+	}
 }
 
 // Called on exit
@@ -103,20 +170,24 @@ void AirGuitarApp::shutdown()
 // Runs update logic
 void AirGuitarApp::update()
 {
-	handsHip = capture.formHandsHip(0);
-
+	
+	gui->update(); 
+	handsHip = capture.formHandsHip(0,rightHanded);
 	if(handsHip != NULL){
+		/*mText = std::to_string(handsHip->chordHandPosition.x) + ", " + std::to_string(handsHip->chordHandPosition.y) + ", " + std::to_string(handsHip->chordHandPosition.z);
+		mText += "\n";
+		mText += std::to_string(handsHip->playingHandPosition.x) + ", " + std::to_string(handsHip->playingHandPosition.y) + ", " + std::to_string(handsHip->playingHandPosition.z);
+		mText += "\n";
+		mText += std::to_string(handsHip->hipPosition.x) + ", " + std::to_string(handsHip->hipPosition.y) + ", " + std::to_string(handsHip->hipPosition.z);
+		mText += "\n";*/
 		mProcessing.calculateVolume(handsHip);
 		mProcessing.calculateTone(handsHip);
 		if(mProcessing.playedNote(handsHip)){
 			mPresentation.playNote(mProcessing.tone,mProcessing.volume);
 		}
-		chordHandPos = capture.SkeletonPointToScreen(handsHip->chordHandPosition, ci::app::getWindowHeight(), ci::app::getWindowWidth());
-		playHandPos = capture.SkeletonPointToScreen(handsHip->playingHandPosition, ci::app::getWindowHeight(), ci::app::getWindowWidth());
-		HipPos = capture.SkeletonPointToScreen(handsHip->hipPosition, ci::app::getWindowHeight(), ci::app::getWindowWidth());
-		mText = std::to_string(mProcessing.tone);
+		//mText += std::to_string(mProcessing.tone);
 	} else {
-		mText = "Skeleton not found";
+		//mText = "Skeleton not found";
 	}
 	ci::gl::Texture *tmp = capture.getNextFrame();
 	if (tmp != NULL){
