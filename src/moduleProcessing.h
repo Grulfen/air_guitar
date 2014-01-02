bb4a3e681e6b1f9e4a5ea024a637aff075b078b3
Carlos Bergen <hola@carlosbergen.com>
2014-01-02 10:32:33 -0300

    Added comments on processing module and removed an unused method

--- src/moduleProcessing.h
+++ src/moduleProcessing.h
@@ -9,23 +9,23 @@ class moduleProcessing{
 
 	public: 
 		moduleProcessing();
-		void calculateTone(HandsHip *handship);
-		void calculateVolume(HandsHip *handship);
-		bool playedNote(HandsHip *handship);
-		float distance(HandsHip *handship);
+		void calculateTone(HandsHip *handship); //Método para calcular el tono
+		void calculateVolume(HandsHip *handship); //Método para calcular el volumen
+		bool playedNote(HandsHip *handship); //Método para ver si una nota fue tocada.
 
+		//Las variables del tono y volumen
 		float volume;
 		int tone;
 
 	private:
-		float E, F, Fsharp, G, Gsharp, A, Asharp, B, C, Csharp, D, Dsharp;
+		float E, F, Fsharp, G, Gsharp, A, Asharp, B, C, Csharp, D, Dsharp; //Las distancias entre en metros entre la cadera y la mano que toca los acordes para cada nota.
 
-		float boxSize;
+		float boxSize; //El tamaño del area de reproducción por donde pasara la mano de reproducción.
 
-		float maxDistance;
+		float maxDistance; //La distancia máxima de la mano de reproducción entre dos frames usado para normalizar el volumen que calculamos en base a la velocidad.
 
-		Vector4 lastPlayingHand;
+		Vector4 lastPlayingHand; //La estructura de la mano de reproducción del frame anterior.
 
-		bool isOnPlayingArea,notePlayed;
+		bool isOnPlayingArea,notePlayed; //Variables que indican si la mano de reproducción se encuentra sobre el área de reproducción y si una nota ya fue tocada.
 					
 };
